55bd8a3ef3da1918cbf1185f4b9868b8418fe372
emmafayepruitt <emmafayepruitt@gmail.com>
2025-11-24 15:35:35 -0500

    Change image paths to GitHub Pages URLs
    Updated image URLs in reasons array to point to the new GitHub Pages location.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const reasons = [
-  { text: "I have 6k trophies in clash", img:"IMG_8990.jpg" },
-  { text: "I’m funnier", img:"IMG_8995.jpg" },
-  { text: "I have great music taste", img:"IMG_9001.jpg" },
-  { text: "I'm NOT chopped", img:"IMG_8998.jpg" },
-  { text: "I have a really cute cat", img:"IMG_9002.jpg" },
-  { text: "I have a very postive attitude", img:"IMG_9013.jpg" },
-  { text: "I’m goated", img:"IMG_9005.jpg" },
-  { text: "I’m beauty", img:"IMG_8992.jpg" },
-  { text: "I'm missing joints in all of my toes", img:"IMG_8996.jpg" },
-  { text: "And lastly.....I LOVE cottage cheese", img:"IMG_8987.jpg" }
+  { text: "I have 6k trophies in clash", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8990.jpg" },
+  { text: "I’m funnier", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8995.jpg" },
+  { text: "I have great music taste", img:"https://emmafayepruitt.github.io/Home-Page/IMG_9001.jpg" },
+  { text: "I'm NOT chopped", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8998.jpg" },
+  { text: "I have a really cute cat", img:"https://emmafayepruitt.github.io/Home-Page/IMG_9002.jpg" },
+  { text: "I have a very postive attitude", img:"https://emmafayepruitt.github.io/Home-Page/IMG_9013.jpg" },
+  { text: "I’m goated", img:"https://emmafayepruitt.github.io/Home-Page/IMG_9005.jpg" },
+  { text: "I’m beauty", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8992.jpg" },
+  { text: "I'm missing joints in all of my toes", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8996.jpg" },
+  { text: "And lastly.....I LOVE cottage cheese", img:"https://emmafayepruitt.github.io/Home-Page/IMG_8987.jpg" }
 ];
 
 let index = -1;
